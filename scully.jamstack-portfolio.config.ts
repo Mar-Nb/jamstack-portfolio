e10f124693e43d585500bbb98c8d44ecfb0873f3
Mar-Nb <niombela.martin@gmail.com>
2022-07-04 23:41:38 +0200

    Premier commit : Mise en place

---
+++ scully.jamstack-portfolio.config.ts
@@ -0,0 +1,29 @@
+import { ScullyConfig } from '@scullyio/scully';
+
+/** this loads the default render plugin, remove when switching to something else. */
+import '@scullyio/scully-plugin-puppeteer'
+
+export const config: ScullyConfig = {
+  projectRoot: "./src",
+  projectName: "jamstack-portfolio",
+  // add spsModulePath when using de Scully Platform Server,
+  outDir: './dist/static',
+  routes: {
+  },
+  puppeteerLaunchOptions: {
+    args: [
+      "--disable-gpu",
+      "--renderer",
+      "--no-sandbox",
+      "--no-service-autorun",
+      "--no-experiments",
+      "--no-default-browser-check",
+      "--disable-dev-shm-usage",
+      "--disable-setuid-sandbox",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-extensions"
+    ]
+  }
+};
